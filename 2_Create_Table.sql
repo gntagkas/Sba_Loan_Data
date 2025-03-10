231476c68a63e3d642974d1bcdd65daef1ec6f02
gntagkas <yiannisntagkas@gmail.com>
2025-03-10 21:14:44 +0200

    Add files via upload

---
+++ 2_Create_Table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE loan_data (
+    loan_id INTEGER PRIMARY KEY,
+    business_name TEXT,
+    loan_amount NUMERIC,
+    loan_status TEXT,
+    city TEXT,
+    state TEXT,
+    approval_date DATE,
+    bank_name TEXT,
+    naics_code NUMERIC,
+    jobs_retained INT
+)
+
+ALTER TABLE public.loan_data OWNER TO postgres;
